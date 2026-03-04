eb745a565928551751c8df8cb4c3f7393f96f2da
tspeakryan <oybeknarzullayev571@gmail.com>
2026-03-04 20:26:41 +0300

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Backend ishlayapti"}
+
+@app.get("/health")
+def health():
+    return {"ok": True}
